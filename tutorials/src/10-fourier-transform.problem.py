dbd7012026538011ed1ccd06f8ced6565f43f60a
quasiAlexAlex <coc.2accksa@gmail.com>
2026-01-12 15:59:53 +0100

    tutorials bis 13

--- tutorials/src/10-fourier-transform.problem.py
+++ tutorials/src/10-fourier-transform.problem.py
@@ -11,30 +11,37 @@ import numpy as np
 window_width = 640
 window_height = 480
 
-# TODO Implement the function get_frequencies(image):
+
+def get_frequencies(image):
 # Convert image to floats and do dft saving as complex output
-
+    dft = cv2.dft(np.float32(image), flags= cv2.DFT_COMPLEX_OUTPUT)
+    
 # Apply shift of origin from upper left corner to center of image
-
+    dft_shift = np.fft.fftshift(dft)
 # Extract magnitude and phase images
-
+    magnitude, phase = cv2.cartToPolar(dft_shift[:, :, 0], dft_shift[:, :, 1])
 # Get spectrum for viewing only
-
+    spec = (1 / 20) * np.log(magnitude)
 # Return the resulting image (as well as the magnitude and phase for the inverse)
+    return spec, magnitude, phase
 
-# TODO Implement the function create_from_spectrum():
+
+def create_from_spectrum(magnitude, phase):
 # Convert magnitude and phase into cartesian real and imaginary components
-
+    real, fake= cv2.polarToCart(magnitude, phase)
 # Combine cartesian components into one complex image
-
+    back = cv2.merge([real, fake])
 # Shift origin from center to upper left corner
-
+    back_ishift = np.fft.ifftshift(back)
 # Do idft saving as complex output
-
+    img_back = cv2.idft(back_ishift)
 # Combine complex components into original image again
-
+    img_back = cv2.magnitude(img_back[:, :, 0], img_back[:, :, 1])
 # Re-normalize to 8-bits
-
+    min, max = np.amin(img_back, (0, 1)), np.amax(img_back, (0, 1))
+    print(min, max)
+    img_back = cv2.normalize(img_back, None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    return img_back
 
 # We use a main function this time: see https://realpython.com/python-main-function/ why it makes sense
 def main():
@@ -43,6 +50,9 @@ def main():
 
     # Load the image.
     image = cv2.imread(image_name, cv2.IMREAD_GRAYSCALE)
+    # check if img is loaded
+    if image is None:
+        raise Exception("Could not read the image.")
     image = cv2.resize(image, (window_width, window_height))
 
     # Show the original image
@@ -53,7 +63,8 @@ def main():
     cv2.imshow(title_original, image)
 
     # result = get_frequencies(image)
-    result = np.zeros((window_height, window_width), np.uint8)
+    # result = np.zeros((window_height, window_width), np.uint8)
+    result, magnitude, phase = get_frequencies(image)
 
     # Show the resulting image
     # Note that window parameters have no effect on MacOS
@@ -63,8 +74,9 @@ def main():
     cv2.imshow(title_result, result)
 
     # back = create_from_spectrum(??)
-    back = np.zeros((window_height, window_width), np.uint8)
-
+    # back = np.zeros((window_height, window_width), np.uint8)
+    back = create_from_spectrum(magnitude, phase)
+    
     # And compute image back from frequencies
     # Note that window parameters have no effect on MacOS
     title_back = "Reconstructed image"
@@ -79,3 +91,4 @@ def main():
 # Starting the main function
 if __name__ == "__main__":
     main()
+
--- tutorials/src/11-transformations.problem.py
+++ tutorials/src/11-transformations.problem.py
@@ -11,45 +11,50 @@ img = cv2.imread("./tutorials/data/images/nl_clown.jpg", cv2.IMREAD_COLOR)
 img = cv2.resize(img, (400, 400), interpolation=cv2.INTER_CUBIC)
 rows, cols, dims = img.shape
 
-# TODO Define translation matrix for translation about 100 pixels to the right and 50 up
-T_translation = 'tbd'
+# Define translation matrix for translation about 100 pixels to the right and 50 up
+T_translation = np.float32([[1, 0, 100], [0, 1, 50]])
 
 # A pretty print for the matrix:
 print("\nTranslation\n", "\n".join(["\t".join(["%03.3f" % cell for cell in row]) for row in T_translation]))
 
-# TODO Apply translation matrix on image using cv2.warpAffine
-dst_translation = 'tbd'
+# Apply translation matrix on image using cv2.warpAffine
+dst_translation = cv2.warpAffine(img, T_translation, (cols + 100, rows + 50))
 
-# TODO Define anisotropic scaling matrix that stretches to double length horizontally
+# Define anisotropic scaling matrix that stretches to double length horizontally
+T_anisotropic_scaling = np.float32([[2, 0, 0], [0, 2, 0]])
 # and squeezes vertically to the half height
-T_anisotropic_scaling = "tbd"
+
 
 print(
     "\nAnisotropic scaling\n",
     "\n".join(["\t".join(["%03.3f" % cell for cell in row]) for row in T_anisotropic_scaling]),
 )
 
-# TODO Apply anisotropic scaling matrix on image using cv2.warpAffine
-dst_anisotropic_scaling = "tbd"
+#  Apply anisotropic scaling matrix on image using cv2.warpAffine
+dst_anisotropic_scaling = cv2.warpAffine(img, T_anisotropic_scaling, (int(cols*2), int(rows*2)))
 
-# TODO Define rotation matrix for 45° clockwise rotation
-T_rotation = "tbd"
+# Define rotation matrix for 45° clockwise rotation
+phi_deg = 45.0
+phi_rad = (np.pi / 180.0) * phi_deg
+T_rotation = np.float32([[np.cos(phi_rad), -np.sin(phi_rad), 0], [np.sin(phi_rad), np.cos(phi_rad), 0]])
 
 print("\nRotation\n", "\n".join(["\t".join(["%03.3f" % cell for cell in row]) for row in T_rotation]))
 
-# TODO Apply rotatio matrix on image using cv2.warpAffine
-dst_rotation = "tbd"
+# Apply rotatio matrix on image using cv2.warpAffine
+dst_rotation = cv2.warpAffine(img, T_rotation, (cols, rows))
 
-# TODO Rotate around image center for 45° counterclockwise using cv2.getRotationMatrix2D
-T_rotation_around_center = "tbd"
+# Rotate around image center for 45° counterclockwise using cv2.getRotationMatrix2D
+center = ((cols - 1) / 2.0, (rows - 1) / 2.0)
+scale_factor = 1
+T_rotation_around_center = cv2.getRotationMatrix2D(center, phi_deg, scale_factor)
 
 print(
     "\nRotation around center\n",
     "\n".join(["\t".join(["%03.3f" % cell for cell in row]) for row in T_rotation_around_center]),
 )
 
-# TODO Apply rotatio matrix on image using cv2.warpAffine
-dst_rotation_around_center = "tbd"
+# Apply rotatio matrix on image using cv2.warpAffine
+dst_rotation_around_center = cv2.warpAffine(img, T_rotation_around_center, (cols, rows))
 
 # Show the original and resulting images
 cv2.imshow("Original", img)
